e4d77169c9611d6723b8aa6ca64703773be158a9
Onuralp <xonuraralpx@gmail.com>
2021-10-20 14:57:13 +0300

    First app without failure

--- FirstCleanApp/ViewController.swift
+++ FirstCleanApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func ChangeButtonClicked(_ sender: Any) {
+        
+        imageView.image = UIImage(named: "2387101_810x458")
+        
+    }
+    
 
 }
 
